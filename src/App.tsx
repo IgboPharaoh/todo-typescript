815accd95b329d61d710cdd4f71c048c58278871
IgboPharaoh <solomoneze62@gmail.com>
2022-04-04 20:50:49 +0100

    setup input field and todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./model";
 
-const App = () => {
-  return <div className="App">Hello world</div>;
-}
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>(" ");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(todos){
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone:false }])
+      setTodo(" ")
+    }
+  }; 
+
+  console.log(todos);
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList/> 
+      {
+        todos.map(t =>(<li>{t.todo}</li>))
+      }
+    </div>
+  );
+};
 
 export default App;
- 
